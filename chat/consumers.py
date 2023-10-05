8e14885d809a406171e58898efdd2cea874a5402
Omnia Osman <UGS.140547@ci.suez.edu.eg>
2023-10-05 15:21:18 +0300

    fix: edit consumers and websockets

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,20 +1,28 @@
+import json 
+import jwt
+from .models import Chat
 from channels.generic.websocket import AsyncWebsocketConsumer
+from django.conf import settings
 from channels.db import database_sync_to_async
 from django.contrib.auth import get_user_model
-from .models import Chat
+from urllib.parse import parse_qs
 
 User = get_user_model()
 
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         # Authenticate the user using JWT token
-        token = self.scope['query_string'].decode().split('=')[1]
-        user = await self.get_user_from_token(token)
-        if user is None:
+        query_string = self.scope['query_string'].decode()
+        token = parse_qs(query_string).get('token', [''])[0]
+        if not token:
             await self.close()
         else:
-            self.user = user
-            await self.accept()
+            user = await self.get_user_from_token(token)
+            if user is None:
+                await self.close()
+            else:
+                self.user = user
+                await self.accept()
 
     async def disconnect(self, close_code):
         # Perform any cleanup tasks when the WebSocket connection is closed
@@ -22,7 +30,16 @@ class ChatConsumer(AsyncWebsocketConsumer):
 
     async def receive(self, text_data):
         # Handle incoming WebSocket messages
-        pass
+        try:
+            data = json.loads(text_data)
+            message = data['message']
+            chat_id = data['chat_id']
+            await self.save_message(chat_id, message)
+            await self.send_message(chat_id, message)
+        except (json.JSONDecodeError, KeyError):
+            # Handle the case when the JSON data is empty or invalid
+            # You can log an error message or take appropriate action here
+            pass
 
     @database_sync_to_async
     def get_user_from_token(self, token):
@@ -32,3 +49,29 @@ class ChatConsumer(AsyncWebsocketConsumer):
             return User.objects.get(id=user_id)
         except (jwt.DecodeError, User.DoesNotExist):
             return None
+
+    @database_sync_to_async
+    def save_message(self, chat_id, message):
+        # Save the message to the Chat model
+        chat = Chat.objects.get(id=chat_id)
+        chat.messages.create(sender=self.user, content=message)
+
+    async def send_message(self, chat_id, message):
+        # Send the message to all connected clients in the chat room
+        await self.channel_layer.group_send(
+            f'chat_{chat_id}',
+            {
+                'type': 'chat_message',
+                'message': message,
+                'sender': self.user.username
+            }
+        )
+
+    async def chat_message(self, event):
+        # Receive the message from the group and send it to the WebSocket
+        message = event['message']
+        sender = event['sender']
+        await self.send(text_data=json.dumps({
+            'message': message,
+            'sender': sender
+        }))
--- project/asgi.py
+++ project/asgi.py
@@ -3,9 +3,9 @@ import django
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from chat.routing import websocket_urlpatterns
+from django.conf import settings
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
-django.setup()
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
